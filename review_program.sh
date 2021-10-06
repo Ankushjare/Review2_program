693d2f7ece2d8ec6568e6213a3f233b5bf1497da
Ankushjare <jareankush@gmail.com>
2021-10-06 11:58:41 +0530

    2review_program

---
+++ review_program.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+  
+echo "write a number between 10 to 20"
+  
+read number;
+
+ echo "you entered $number";
+   
+for((counter=$number; counter>=1; counter--))
+do
+ echo "$counter";
+done
