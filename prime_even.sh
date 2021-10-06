7eb211b7a9f588fa9198908e95c88d406f08d540
Ankushjare <91589773+Ankushjare@users.noreply.github.com>
2021-10-06 12:24:29 +0530

    Update prime_even.sh

--- prime_even.sh
+++ prime_even.sh
@@ -3,6 +3,14 @@
 echo "enter number"
 read num
 
+value=$(($num%2))
+if [ $value -eq 0 ]
+then
+ echo "even";
+else
+ echo "odd"
+fi
+
 for((i=2; i<=num/2; i++))
 do
   if [ $((num%i)) -eq 0 ]
@@ -14,10 +22,4 @@ done
 echo "$num is a prime number."
 
 
-value=$(($num%2))
-if [ $value -eq 1 ]
-then
- echo "odd";
-else
- echo "even"
-fi
+
